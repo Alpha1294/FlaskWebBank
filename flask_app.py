74ab21849c3dbabcfcb231d54629bf69e2ab7d8a
Gabriel Radu <radugabriel1294@gmail.com>
2023-01-20 17:20:58 +0100

    port variable added and debugg false

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,8 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = os.environ.get("PORT",5000) #Heroku will set the port envoronment variable for web traffic
+    app.run(debug=False, host="0.0.0.0", port=port)
